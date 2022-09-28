b8affebab5340d557ec1d0606902b3ef5ed023b5
Nunzio Baldares <bvldares@outlook.com>
2022-09-28 02:27:50 +0200

    bog

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 const btnEl = document.querySelectorAll(".btn")
 const inputCash = document.getElementById("calculator-input-amount")
-const inputPeople = document.getElementById("calculator-input-people")
-
+const inputPeople = document.getElementById("calculator-input-people") 
 const tipEl = document.getElementById("tip")
 const totalEl = document.getElementById("total")
 
 
 btnEl.forEach(function(btn){
-    btn.addEventListener("click", function(){
-       tipEl.textContent = tipCalculator(btn.value, inputPeople.value, inputCash.value)
+    btn.addEventListener("click", function(){ 
+       tipEl.textContent = tipCalculator(btn.value, inputCash.value, inputPeople.value)
+       totalEl.textContent =  totalCalculator(inputCash.value, tipCalculator(btn.value, inputCash.value, inputPeople.value), inputPeople.value)
     })
 })
 
-
-
-
-function tipCalculator(percentage, people, total){
-    return (total * percentage) / people
+function tipCalculator(percentage, total, people){
+    return (total * (percentage /100)) / people
 }
-
 
 function totalCalculator(total, tip, people){
-    return (total + tip) / people
+    return total / people + tip 
 }
+
+
+document.querySelector(".btn-keyup").addEventListener("keyup", (event)=>{
+    tipEl.textContent = tipCalculator(event.target.value, inputCash.value, inputPeople.value)
+    totalEl.textContent =  totalCalculator(inputCash.value, tipCalculator(event.target.value, inputCash.value, inputPeople.value), inputPeople.value)
+})
+
+
+document.getElementById("reset").addEventListener("click", ()=>{
+    inputCash.value = 0
+    inputPeople.value = 0
+    tipEl.textContent = 0
+    totalEl.textContent = 0
+})
